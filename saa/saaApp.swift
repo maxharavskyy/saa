e022f9537c6541b604b4885b79bbfd0a17f883fd
Max Haravskyy <haravskyy@gmail.com>
2023-03-01 21:26:45 +0200

    Initial Commit

---
+++ saa/saaApp.swift
@@ -0,0 +1,17 @@
+//
+//  saaApp.swift
+//  saa
+//
+//  Created by Max Haravskyy on 01.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct saaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
